b838ed1e3dc6aa725dd12bd5dfb46a85445ec538
medamshiva20 <medamshiva20@gmail.com>
2026-03-09 22:48:58 +0530

    aws route table association

--- main.tf
+++ main.tf
@@ -150,7 +150,7 @@ resource "aws_route_table_association" "public"{
 
 resource "aws_route_table_association" "private"{
     count = length(var.private_subnet_cidrs)
-    subnet_id = aws.subnet.private[count.index].id
+    subnet_id = aws_subnet.private[count.index].id
     route_table_id = aws_route_table.private.id
 }
 
