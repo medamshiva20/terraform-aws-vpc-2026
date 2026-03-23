e31fb238a411316bef317cb00a856adb0b7d83f4
medamshiva20 <medamshiva20@gmail.com>
2026-03-23 19:06:21 +0530

    peering connection code added

--- data.tf
+++ data.tf
@@ -1,3 +1,12 @@
 data "aws_availability_zones" "available"{
     state = "available"
 }
+
+#If it is default vpc we can use below method 
+#data "aws_vpc" "default" {
+ # default = true
+#}
+
+data "aws_vpc" "prod-vpc"{
+   id = "vpc-072112754da905ebf"
+}
---
+++ peering.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc_peering_connection" "default" {
+
+   count = var.is_peering_required ? 1 : 0
+  #peer_owner_id = var.peer_owner_id
+
+  #Acceptor
+  peer_vpc_id   = data.aws_vpc.prod-vpc.id
+
+  #Requestor
+  vpc_id        = aws_vpc.main.id
+
+  auto_accept = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
