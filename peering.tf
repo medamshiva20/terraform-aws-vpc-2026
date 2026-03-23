7c87b6487ff3ebbb457d830e332a2e8213a3e40e
medamshiva20 <medamshiva20@gmail.com>
2026-03-23 21:47:15 +0530

    adding routes to route table

--- peering.tf
+++ peering.tf
@@ -33,3 +33,24 @@ resource "aws_route" "prod_vpc_peering"{
     destination_cidr_block = var.cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
+
+resource "aws_route" "public_peering"{
+    count = var.is_peering_required ? 1 : 0
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = data.aws_vpc.prod-vpc.cidr_block
+    vpc_peering_connection = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private_peering"{
+    count = var.is_peering_required ? 1 : 0
+    route_table_id = aws_route_table.private.id
+    destination_cidr_block = data.aws_vpc.prod-vpc.cidr_block
+    vpc_peering_connection.id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_peering"{
+    count = var.is_peering_required ? 1 : 0
+    route_table_id = aws_route_table.database.id
+    destination_cidr_block = data.aws_vpc.prod-vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
