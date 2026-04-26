5acf7689eda295c73339d0dc0e52e02222c0c160
medamshiva20 <medamshiva20@gmail.com>
2026-04-26 19:51:41 +0530

    vpc peering

--- data.tf
+++ data.tf
@@ -3,16 +3,16 @@ data "aws_availability_zones" "available"{
 }
 
 #If it is default vpc we can use below method 
-#data "aws_vpc" "default" {
- # default = true
-#}
-
-data "aws_vpc" "prod_vpc"{
-   id = "vpc-02369099af5a5808c"
+data "aws_vpc" "default" {
+  default = true
 }
 
+#data "aws_vpc" "prod_vpc"{
+ #  id = "vpc-02369099af5a5808c"
+#}
+
 data "aws_route_table" "prod"{
-    vpc_id = data.aws_vpc.prod_vpc.id
+    vpc_id = data.aws_vpc.default.id
     filter {
         name = "association.main"
         values = ["true"]
